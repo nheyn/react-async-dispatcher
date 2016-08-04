785b81c18105fe62f9a577b9352bbac9ee4fd275
nheyn <nick@heyn.io>
2016-08-03 19:17:07 -0600

    Add 'dispatchWhenMounted' Higher Order Components
    Added a new hoc that will dispatch actions when the component is mounted.

---
+++ src/dispatchWhenMounted.js
@@ -0,0 +1,38 @@
+/* @flow */
+import React from 'react';
+
+import getDispatcher from './getDispatcher';
+
+import type { Action, Dispatcher } from 'async-dispatcher';
+import type { HigherOrderComponent } from './index';
+
+/**
+ *
+ */
+export default function dispatchWhenMounted(
+  action: Action | Array<Action>,
+  dispatcher?: Dispatcher
+): HigherOrderComponent {
+  const actions = Array.isArray(action)? action: [action];
+
+  return (Component) => {
+    class DispatchWhenMounted extends React.Component<*, *, *> {
+      componentDidMount() {
+        const currDispatcher = getDispatcher(this.props, this.context, dispatcher);
+
+        for(let i=0; actions.length>i; i++) {
+          currDispatcher.dispatch(action);
+        }
+      }
+
+      render(): React.Element<*> {
+        return <Component {...this.props} />;
+      }
+    }
+    DispatchWhenMounted.childContextTypes = {
+      dispatcher: React.PropTypes.object,
+    };
+
+    return DispatchWhenMounted;
+  };
+}
